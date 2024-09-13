de2a226605d4726d611b6955d55ada104029e5ed
Mostafa-Awaad <mostafamohamedkamal2000@gmail.com>
2024-09-13 19:02:16 +0300

    Adding new algorithm to find out when the function return 0

--- Permutation_of_Strings/permutation.c
+++ Permutation_of_Strings/permutation.c
@@ -9,23 +9,35 @@ int next_permutation(int n, char **s)
 	* Return 0 when there is no next permutation and 1 otherwise
 	* Modify array s to its next permutation
 	*/
-    int Iter = 0;
-    int flag = 0;
+    int Iter1 = 0, Iter2 = 0;
     /* Adding the scenario where the function will stop if it reaches it
        if the first element in the array of strings is larger than the rest the function will return 0 */
-    for (Iter = 0 ; Iter < n ; Iter++)
+    for (Iter1 = 0, Iter2 = Iter1+1 ; Iter2 < n-1 ; Iter1++)
     {
-        if (s[0] <= s[Iter]) {flag = 0;}
-        else {flag = 1;}
+        if ((s[Iter1] > s[Iter1+1]) && (s[Iter2] > s[Iter2+1])) return 0;
+        else {break;}
     }
-    if (flag == 1)
-    {
-        return 0;
-    }
-    else
+    
+    if (s[n-1] < s[n-2])
     {
 
     }
+    else
+    {
+        for (Iter1 = 0 ; Iter1 < n-1 ; Iter1 ++)
+        {
+            if ((strcmp(s[Iter1], s[0]) != 0))
+            {
+                char *temp = s[Iter1]; 
+                s[Iter1] = s[Iter1+1]; 
+                s[Iter1+1] = temp;
+                return 1; 
+            }
+        }
+       
+       
+    }
+
 }
 
 int main()
