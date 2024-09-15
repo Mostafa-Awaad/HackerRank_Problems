4525aa1bd5f08ead1618383d721441c4742196a4
Mostafa-Awaad <mostafamohamedkamal2000@gmail.com>
2024-09-15 03:06:14 +0300

    Implement the sorting function based on the number of distinct and add some documentations

--- Sorting_Array_of_Strings/sorting.c
+++ Sorting_Array_of_Strings/sorting.c
@@ -3,14 +3,14 @@
 #include <string.h>
 
 /**
- * @brief   :   Strings are arranged from "smallest" to "largest" based on their lexicographical comparison
+ * @brief     Strings are arranged from "smallest" to "largest" based on their lexicographical comparison
  * 
- * @param  a: First string 
- * @param  b: Second string
+ * @param  a  First string 
+ * @param  b  Second string
  * 
- * @retval  : -1 if a is lexicographically smaller than b
- *             0 if two strings are equal
- *             1 if a is lexicographically greater than b
+ * @retval    -1 if a is lexicographically smaller than b
+ *            0 if two strings are equal
+ *            1 if a is lexicographically greater than b
  */
 int lexicographic_sort(const char* a, const char* b) 
 {
@@ -18,31 +18,94 @@ int lexicographic_sort(const char* a, const char* b)
 }
 
 /**
- * @brief   :   Strings are arranged from "smallest" to "largest" based on their lexicographical comparison
+ * @brief     Strings are arranged from "smallest" to "largest" based on their lexicographical comparison
  * 
- * @param  a: First string 
- * @param  b: Second string
+ * @param  a  First string 
+ * @param  b  Second string
  * 
- * @retval  : -1 if b is lexicographically smaller than a
- *             0 if two strings are equal
- *             1 if b is lexicographically greater than a
+ * @retval    -1 if b is lexicographically smaller than a
+ *            0 if two strings are equal
+ *            1 if b is lexicographically greater than a
  */
 int lexicographic_sort_reverse(const char* a, const char* b) 
 {
     return strcmp(b, a);
 }
 
-int sort_by_number_of_distinct_characters(const char* a, const char* b) {
+/**
+ * @brief    Compare two strings based on their number of distinct elements and sort them in non-decreasing order.
+ * 
+ * @param a  A pointer to the first string to be compared.
+ * @param b  A pointer to the second string to be compared.
+ * 
+ * @return   An integer less than, equal to, or greater than zero if the first string is found,
+ *           respectively, to have less distinct chars, same number of distinct chars, or have more distinct chars than the second string. 
+ *           If the number of distinct chars are equal, the result of `strcmp(a, b)` is returned.
+ */
+int sort_by_number_of_distinct_characters(const char* a, const char* b) 
+{
+    int Iter = 0;
+
+    int Distinct1 = 0;  /*Variable to hold the number of distinct chars in str a*/
+    int Distinct2 = 0;  /*Variable to hold the number of distinct chars in str b*/
+
+    int s1[2500] = {0}; /*To hold position of distinct characters for str a */
+    int s2[2500] = {0}; /*To hold position of distinct characters for str b */
+
+    for (Iter = 0 ; a[Iter] != '\0' ; Iter++)
+    {
+        /* Assign 1 in the new int array in the position of last occurence of current character*/
+        s1[(strchr(a,a[Iter]) - a)] = 1;
     
+    }
+    /* Calculate the number of distinct element in str a */
+    for (Iter = 0 ; Iter < 2500 ; Iter++)
+    {
+        if (s1[Iter] == 1)
+        {
+            Distinct1++;
+        }
+    }
+
+    for (Iter = 0 ; b[Iter] != '\0' ; Iter++)
+    {
+        /* Assign 1 in the new int array in the position of last occurence of current character*/
+        s2[(strchr(b,b[Iter]) - b)] = 1;
+    
+    }
+    /* Calculate the number of distinct element in str a */
+    for (Iter = 0 ; Iter < 2500 ; Iter++)
+    {
+        if (s2[Iter] == 1)
+        {
+            Distinct2++;
+        }
+    }
+
+    if (Distinct1 != Distinct2)
+    {
+        return (Distinct1 - Distinct2);
+    }
+    strcmp(a,b);   
 }
 
+/**
+ * @brief    Compare two strings based on their length and sort them in non-decreasing order.
+ * 
+ * @param a  A pointer to the first string to be compared.
+ * @param b  A pointer to the second string to be compared.
+ * 
+ * @return   An integer less than, equal to, or greater than zero if the first string is found,
+ *           respectively, to be shorter, of the same length, or longer than the second string. 
+ *           If the lengths are equal, the result of `strcmp(a, b)` is returned.
+ */
 int sort_by_length(const char* a, const char* b) 
 {
     int Str_Len1 = strlen(a);
     int Str_Len2 = strlen(b);
 
     if (Str_Len1 != Str_Len2)
-        return Str_Len1 - Str_Len2;
+        return (Str_Len1 - Str_Len2);
     else
         return strcmp(Str_Len1,Str_Len2);
 }
