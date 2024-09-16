73cc2cada59e8dd0ed727d9c1536ce61e32e81d8
Mostafa-Awaad <mostafamohamedkamal2000@gmail.com>
2024-09-16 14:53:27 +0300

    Ignore main

--- Sorting_Array_of_Strings/sorting.c
+++ Sorting_Array_of_Strings/sorting.c
@@ -196,23 +196,3 @@ void string_sort(char** arr,const int len,int (*cmp_func)(const char* a, const c
     }
     else {printf("Enter a valid sorting method\n");}
 }
-
-int main() 
-{
-    int n;
-    scanf("%d", &n);
-  
-    char** arr;
-	arr = (char**)malloc(n * sizeof(char*));
-  
-    for(int i = 0; i < n; i++){
-        *(arr + i) = malloc(1024 * sizeof(char));
-        scanf("%s", *(arr + i));
-        *(arr + i) = realloc(*(arr + i), strlen(*(arr + i)) + 1);
-    }
-  
-    string_sort(arr, n, lexicographic_sort);
-    for(int i = 0; i < n; i++)
-        printf("%s\n", arr[i]);
-    printf("\n");
-}
