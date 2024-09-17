c98dd677b6b68a0867e623d7de99efa901b6bb19
Mostafa-Awaad <mostafamohamedkamal2000@gmail.com>
2024-09-18 00:35:31 +0300

    Add get_document implementation but there is an issue in it that we will isa solve it in the next try

--- Querying_the_document/querying.c
+++ Querying_the_document/querying.c
@@ -21,6 +21,53 @@ char*** kth_paragraph(char**** document, int k)
 }
 
 
-char**** get_document(char* text) {
+char**** get_document(char* text) 
+{
+    /* Allocation of memory block for document and suppose at beginning has only one paragraph*/
+    char ****document = (char ****)malloc(sizeof(char ***));
+    int parag_no = 0; // for storing number of paragraphs
+    
+    /*Tokenization of our text into paragraphs*/
+    char *paragraph_token = strtok(text, "\n");
+    while (paragraph_token != NULL)
+    {
+        parag_no++;
+        /*Reallocation and expanding our memory block based on number of paragraphs found*/
+        document = realloc(document, parag_no * sizeof(char ***));
+        
+        /* Allocation of memory block for paragraph and suppose at beginning by one sentence*/
+        char ***paragraph = (char ***)malloc(sizeof(char **));
+        int sent_no = 0; // for storing number of sentences
 
+        /* Tokenization of our paragraph into sentences*/
+        char *sentence_token = strtok(paragraph_token,".");
+        while (sentence_token != NULL)
+        {
+            sent_no++;
+            /*Reallocation and expanding our memory block based on number of sentences found*/
+            paragraph = realloc(paragraph, sent_no * sizeof(char **));
+            /* Allocation of memory block for sentence */
+            char **sentence = (char **)malloc(sizeof(char *));
+            int word_no = 0;// for storing number of words in one sentence
+
+            /*Tokenization of our sentence into words*/
+            char *word_token = strtok(sentence_token, " ");
+            while (word_token != NULL)
+            {
+                word_no++;
+                /*Realloction of memory block of words*/
+                sentence = realloc(sentence, word_no * sizeof(char *));
+                sentence[word_no-1] = word_token;
+                word_token = strtok(NULL, " ");
+            }
+            
+            paragraph[sent_no-1] = sentence;
+            sentence_token = strtok(NULL, ".");
+        }
+        document[parag_no-1] = paragraph;
+        paragraph_token = strtok(NULL, "\n");
+        
+    }
+
+    return document;
 }
